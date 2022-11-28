52884ed4f307728953ba47f62a4648d8733bec07
(Ben Kaefer)benkaefer@icloud.com <benkaefer@icloud.com>
2022-11-28 16:15:10 +0000

    done with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_152844) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_161308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boroughs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username"
+    t.string "postcode"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "borough_id", null: false
+    t.index ["borough_id"], name: "index_users_on_borough_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "boroughs"
 end
