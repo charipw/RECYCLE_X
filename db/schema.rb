9dd37545037ad8534ad8a8efa59e429b439948d8
Charleen Ipping Wah <charleen.ipping@gmail.com>
2022-11-29 12:54:08 +0000

    Added borough as foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_125009) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_125210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_125009) do
     t.string "is_recycled"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "borough_id", null: false
+    t.index ["borough_id"], name: "index_rules_on_borough_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,5 +58,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_125009) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rules", "boroughs"
   add_foreign_key "users", "boroughs"
 end
