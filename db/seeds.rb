7f0fba66a9c9db3942e9a5bc9762117c935023f4
Pia Holtappels <pia.holtappels@icloud.com>
2022-11-29 15:19:53 +0000

    fixed login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+
+User.destroy_all
+Borough.destroy_all
+
 
 Packaging.create(
   [
