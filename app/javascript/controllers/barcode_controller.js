ca774a77278a0356b632e79cd7db2f233b83bf12
(Ben Kaefer)benkaefer@icloud.com <benkaefer@icloud.com>
2022-11-30 14:46:09 +0000

    able to display in console

--- app/javascript/controllers/barcode_controller.js
+++ app/javascript/controllers/barcode_controller.js
@@ -4,33 +4,52 @@ import { Html5QrcodeScanner} from "html5-qrcode";
 // Connects to data-controller="barcode"
 export default class extends Controller {
   connect() {
-    console.log("hello world")
-    var resultContainer = document.getElementById('qr-reader-results');
-    var lastResult, countResults = 0;
+    let lastResult, countResults = 0;
 
-  function onScanSuccess(decodedText, decodedResult) {
-      if (decodedText !== lastResult) {
-          ++countResults;
-          lastResult = decodedText;
-          // Handle on success condition with the decoded message.
-          console.log(`Scan result ${decodedText}`, decodedResult);
+    const onScanSuccess = (decodedText, decodedResult) => {
 
-          var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
+        if (countResults === 0) {
+            ++countResults;
+            lastResult = decodedText;
+            // Handle on success condition with the decoded message.
+            console.log(`Scan result ${decodedText}`, decodedResult);
+            html5QrcodeScanner.clear();
 
-          fetch(`https://world.openfoodfacts.org/api/v0/product/${decodedText}.json`, requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
+            const requestOptions = {
+              method: 'GET',
+              redirect: 'follow'
+            };
 
-      }
-  }
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${decodedText}.json`, requestOptions)
+              .then(response => response.json())
+              .then(result => {
+                console.log(result["product"]);
+                console.log(result["product"]["brands"]);
+                console.log(result["product"]["packaging"]);
+                console.log(result["product"]["packaging_tags"]);
+                console.log(result["product"]["ecoscore_grade"]);
 
-  var html5QrcodeScanner = new Html5QrcodeScanner(
+                "https://images.openfoodfacts.org/images/products/005/557/742/0249/front_fr.9.200.jpg"
+
+
+              })
+
+
+              // .catch(error => console.log('error', error));
+              // console.log(typeof(result))
+
+        }
+        else {
+          Html5QrcodeScanner.stop().then((ignore) => {
+            // QR Code scanning is stopped.
+          }).catch((err) => {
+            // Stop failed, handle it.
+          });
+        }
+    }
+    const html5QrcodeScanner = new Html5QrcodeScanner(
       "qr-reader", { fps: 10, qrbox: 250 });
-  html5QrcodeScanner.render(onScanSuccess);
+      html5QrcodeScanner.render(onScanSuccess);
     }
 
   }
