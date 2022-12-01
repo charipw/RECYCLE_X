f294e663fcbcc27908ee9941ae2be65f37faaef0
Charleen Ipping Wah <charleen.ipping@gmail.com>
2022-12-01 11:02:18 +0000

    Removed PG search from Packaging mode

--- app/models/packaging.rb
+++ app/models/packaging.rb
@@ -1,10 +1,10 @@
 class Packaging < ApplicationRecord
   self.inheritance_column = :_type_disabled
 
-  include PgSearch::Model
-  pg_search_scope :search_by_category_and_type,
-    against: [ :category, :type ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_category_and_type,
+  #   against: [ :category, :type ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  # }
 end
