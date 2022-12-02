5668df33a4979ad034b3fb5543b0e8695dd9f272
Charleen Ipping Wah <charleen.ipping@gmail.com>
2022-12-02 10:54:33 +0000

    Cmmenting a method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,18 +30,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  def create
-    @item = Item.new
-    @item.name = item_params[:name]
-    @item.name = item_params[:bar_code]
-    @item.name = item_params[:eco_score]
-    @item.save
-    item_params[:type].each do |type|
-      @packaging = Packaging.find_by(type)
-      @packaging = Packaging.find_by(type: 'Unknown') if @packaging.empty
-      ItemPackaging.create(item: @item, packaging: @packaging)
-    end
-  end
+  # def create
+  #   @item = Item.new
+  #   @item.name = item_params[:name]
+  #   @item.name = item_params[:bar_code]
+  #   @item.name = item_params[:eco_score]
+  #   @item.save
+  #   item_params[:type].each do |type|
+  #     @packaging = Packaging.find_by(type)
+  #     @packaging = Packaging.find_by(type: 'Unknown') if @packaging.empty
+  #     ItemPackaging.create(item: @item, packaging: @packaging)
+  #   end
+  # end
   # end
 
   # private
